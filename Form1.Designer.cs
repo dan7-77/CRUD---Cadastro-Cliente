b83af9be78a6f26714a65ba760cbffe7ca84edab
dan7-77 <danieabreu151@gmail.com>
2024-11-12 10:29:36 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Crud___Cadastro_de_Produto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnome = new System.Windows.Forms.Label();
+            this.lblcod = new System.Windows.Forms.Label();
+            this.lblpreço = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblnome
+            // 
+            this.lblnome.AutoSize = true;
+            this.lblnome.Location = new System.Drawing.Point(97, 28);
+            this.lblnome.Name = "lblnome";
+            this.lblnome.Size = new System.Drawing.Size(38, 13);
+            this.lblnome.TabIndex = 0;
+            this.lblnome.Text = "Nome:";
+            // 
+            // lblcod
+            // 
+            this.lblcod.AutoSize = true;
+            this.lblcod.Location = new System.Drawing.Point(96, 74);
+            this.lblcod.Name = "lblcod";
+            this.lblcod.Size = new System.Drawing.Size(90, 13);
+            this.lblcod.TabIndex = 2;
+            this.lblcod.Text = "Codigo de barras:";
+            // 
+            // lblpreço
+            // 
+            this.lblpreço.AutoSize = true;
+            this.lblpreço.Location = new System.Drawing.Point(97, 126);
+            this.lblpreço.Name = "lblpreço";
+            this.lblpreço.Size = new System.Drawing.Size(38, 13);
+            this.lblpreço.TabIndex = 3;
+            this.lblpreço.Text = "Preço:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblpreço);
+            this.Controls.Add(this.lblcod);
+            this.Controls.Add(this.lblnome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnome;
+        private System.Windows.Forms.Label lblcod;
+        private System.Windows.Forms.Label lblpreço;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
